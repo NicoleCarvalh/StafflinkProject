8d638be81928faa0e51091d47bfb5017b3c53224
Arthur-Mendes-M <arthurmendesmartins0105@gmail.com>
2023-07-28 22:06:44 -0300

    Objetivo: implementacao dos conteudos das paginas de controle de ponto e financas.

--- pages/attendance/scripts/main.js
+++ pages/attendance/scripts/main.js
@@ -2,3 +2,5 @@ import { allUtils } from "../../../patternScripts/main.js";
 
 allUtils.sideMenu()
 allUtils.notes()
+
+alert('Versão diferente para o usuário específico ainda não desenvolvido')
--- pages/finances/scripts/main.js
+++ pages/finances/scripts/main.js
@@ -2,3 +2,5 @@ import { allUtils } from "../../../patternScripts/main.js";
 
 allUtils.sideMenu()
 allUtils.notes()
+
+alert('Página em desenvolvimento')
