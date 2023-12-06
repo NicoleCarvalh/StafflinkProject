c3118d2ef9e4f36c9eac58af6edda1a4e47161e8
Arthur-Mendes-M <arthurmendesmartins0105@gmail.com>
2023-12-06 13:21:58 -0300

    Objetivo: implementando alerta de pagina em desenvolvimento - pagina de esqueceu a senha

--- pages/forgotPassword/scripts/main.js
+++ pages/forgotPassword/scripts/main.js
@@ -1,3 +1,7 @@
 import { allUtils } from "../../../patternScripts/main.js";
 
 // allUtils.sideMenu()
+
+allUtils.toastAlert({message: 'Atenção!', description: 'Essa página está em desenvolvimento.', className: 'danger', options: {duration: -1, close: false}}).then(() => {
+    window.location.href = '/'
+})
